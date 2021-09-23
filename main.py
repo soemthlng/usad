352a63c115298e6e5b8fcf3b9ee5dd9c38df16d0
soemthlng <yonghome93@gmail.com>
2021-09-23 14:36:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,72 +18,87 @@ def dataframe_from_csv(target):
 def dataframe_from_csvs(targets):
     return pd.concat([dataframe_from_csv(x) for x in targets])
 
-normal_dataset = sorted([x for x in Path('input/data_20/training/').glob("*.csv")])
-normal = dataframe_from_csvs(normal_dataset)
-normal = normal.drop(["time"] , axis = 1)
-normal.shape
-
-for i in list(normal):
-    normal[i]=normal[i].apply(lambda x: str(x).replace("," , "."))
-normal = normal.astype(float)
-
-normal.shape
-
 min_max_scaler = preprocessing.MinMaxScaler()
 
-x = normal.values
+# TRAIN
+train_dataset = sorted([x for x in Path('input/training/').glob("*.csv")])
+train = dataframe_from_csvs(normal_dataset)
+train = train.drop(["time"] , axis = 1)
+train.shape
+
+for i in list(train):
+    train[i]=train[i].apply(lambda x: str(x).replace("," , "."))
+train = train.astype(float)
+train.shape
+
+x = train.values
 x_scaled = min_max_scaler.fit_transform(x)
-normal = pd.DataFrame(x_scaled)
-normal.head(2)
+train = pd.DataFrame(x_scaled)
 
-attack_dataset = sorted([x for x in Path('input/data_20/validation/').glob("*.csv")])
-attack = dataframe_from_csvs(attack_dataset)
-labels = [ float(label!= 0 ) for label  in attack["attack"].values]
-attack = attack.drop(["time", "attack"] , axis = 1)
-attack.shape
+#TEST
+test_dataset = sorted([x for x in Path('input/data_new/testing/').glob("*.csv")])
+test = dataframe_from_csvs(test_dataset)
+test = test.drop(["time"], axis=1)
+print("test shape :        ", test.shape)
 
-for i in list(attack):
-    attack[i]=attack[i].apply(lambda x: str(x).replace("," , "."))
-attack = attack.astype(float)
+for i in list(test):
+    test[i]=test[i].apply(lambda x: str(x).replace(",", "."))
+test = test.astype(float)
 
-attack.shape
-
-x = attack.values
-
+x = test.values
 x_scaled = min_max_scaler.transform(x)
-attack = pd.DataFrame(x_scaled)
+pd.set_option('display.max_columns', 0)
+test = pd.DataFrame(x_scaled)
 
-attack.head(2)
+#VALIDATION
+validation_dataset = sorted([x for x in Path('input/data_new/validation/').glob("*.csv")])
+validation = dataframe_from_csvs(validation_dataset)
+labels = [ float(label!= 0 ) for label  in attack["attack"].values]
+validation = validation.drop(["time", "attack"] , axis = 1)
+print("validation shape :        ", validation.shape)
 
-window_size=12
+for i in list(validation):
+    validation[i]=validation[i].apply(lambda x: str(x).replace("," , "."))
+validation = validation.astype(float)
 
-windows_normal=normal.values[np.arange(window_size)[None, :] + np.arange(normal.shape[0]-window_size)[:, None]]
-windows_normal.shape
+
+x = validation.values
+x_scaled = min_max_scaler.transform(x)
+validation = pd.DataFrame(x_scaled)
 
 
-windows_attack=attack.values[np.arange(window_size)[None, :] + np.arange(attack.shape[0]-window_size)[:, None]]
-windows_attack.shape
+# WINDOW_SIZE must be lower than 8
+WINDOW_SIZE=7
+
+windows_train=normal.values[np.arange(WINDOW_SIZE)[None, :] + np.arange(normal.shape[0]-WINDOW_SIZE)[:, None]]
+windows_train.shape
+
+windows_test=test.values[np.arange(WINDOW_SIZE)[None, :] + np.arange(test.shape[0]-WINDOW_SIZE)[:, None]]
+windows_test.shape
+
+windows_validation=validation.values[np.arange(WINDOW_SIZE)[None, :] + np.arange(attack.shape[0]-WINDOW_SIZE)[:, None]]
+windows_validation.shape
 
 BATCH_SIZE =  500
 N_EPOCHS = 50
 hidden_size = 100
 
-w_size=windows_normal.shape[1]*windows_normal.shape[2]
-z_size=windows_normal.shape[1]*hidden_size
+w_size=windows_train.shape[1]*windows_train.shape[2]
+z_size=windows_train.shape[1]*hidden_size
 
-windows_normal_train = windows_normal[:int(np.floor(.8 *  windows_normal.shape[0]))]
-windows_normal_val = windows_normal[int(np.floor(.8 *  windows_normal.shape[0])):int(np.floor(windows_normal.shape[0]))]
+windows_normal_train = windows_train[:int(np.floor(.8 *  windows_train.shape[0]))]
+windows_normal_test = windows_test[int(np.floor(.8 *  windows_test.shape[0])):int(np.floor(windows_test.shape[0]))]
 
 train_loader = torch.utils.data.DataLoader(data_utils.TensorDataset(
-    torch.from_numpy(windows_normal_train).float().view(([windows_normal_train.shape[0],w_size]))
+    torch.from_numpy(windows_train).float().view(([windows_train.shape[0],w_size]))
 ) , batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
 
 val_loader = torch.utils.data.DataLoader(data_utils.TensorDataset(
-    torch.from_numpy(windows_normal_val).float().view(([windows_normal_val.shape[0],w_size]))
+    torch.from_numpy(windows_test).float().view(([windows_test.shape[0],w_size]))
 ) , batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
 
 test_loader = torch.utils.data.DataLoader(data_utils.TensorDataset(
-    torch.from_numpy(windows_attack).float().view(([windows_attack.shape[0],w_size]))
+    torch.from_numpy(windows_validation).float().view(([windows_validation.shape[0],w_size]))
 ) , batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
 
 model = UsadModel(w_size, z_size)
@@ -110,8 +125,8 @@ results=testing(model,test_loader)
 
 
 windows_labels=[]
-for i in range(len(labels)-window_size):
-    windows_labels.append(list(np.int_(labels[i:i+window_size])))
+for i in range(len(labels)-WINDOW_SIZE):
+    windows_labels.append(list(np.int_(labels[i:i+WINDOW_SIZE])))
 
 y_test = [1.0 if (np.sum(window) > 0) else 0 for window in windows_labels ]
 
