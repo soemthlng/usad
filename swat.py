bb56fa9d034cee225b2a478a0b952ec363dd3fd4
soemthlng <yonghome93@gmail.com>
2022-02-10 15:42:05 +0900

    Update swat.py

--- swat.py
+++ swat.py
@@ -4,112 +4,64 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import torch
 import torch.nn as nn
+import torch.utils.data as data_utils
+
 
 from utils import *
-#from usad import *
-from usad_backup import *
-from sklearn.metrics import *
-from pathlib import Path
-from TaPR_pkg import etapr
-from sklearn.decomposition import PCA
+from usad import *
 from sklearn import preprocessing
+from sklearn.metrics import classification_report
+from sklearn.metrics import *
+
+device = get_default_device()
 
 def put_labels(distance, threshold):
     xs = np.zeros_like(distance)
     xs[distance > threshold] = 1
     return xs
 
-def dataframe_from_csv(target):
-    return pd.read_csv(target).rename(columns=lambda x: x.strip())
-
-def dataframe_from_csvs(targets):
-    return pd.concat([dataframe_from_csv(x) for x in targets])
-
-#Read data
-# SWaT
 normal = pd.read_csv("input/SWaT_Dataset_Normal_v1.csv")#, nrows=1000)
 normal = normal.drop(["Timestamp" , "Normal/Attack" ] , axis = 1)
-#normal.shape
 
-# HAI
-#normal_dataset = sorted([x for x in Path('input_HAI/').glob("train*.csv")])
-#normal = dataframe_from_csvs(normal_dataset)
-#normal = normal.drop(["time", "P1_B2004", "P1_B2016", "P1_B4002", "P1_B4022", "P1_FCV02Z", "P1_PCV02D", "P1_PP01AD", "P1_PP01AR", "P1_PP01BD", "P1_PP01BR", "P1_PP02D", "P1_PP02R", "P1_STSP", "P2_ASD", "P2_AutoGO", "P2_Emerg", "P2_ManualGO", "P2_OnOff", "P2_RTR", "P2_TripEx", "P2_VTR01", "P2_VTR02", "P2_VTR03", "P2_VTR04", "P3_LH", "P3_LL", "P4_HT_FD", "P4_ST_FD", "attack", "attack_P1", "attack_P2", "attack_P3"], axis=1)
-#normal = normal.drop(["time", "C02", "C03", "C06", "C08", "C09", "C10", "C11", "C14", "C18", "C19", "C21", "C22", "C33", "C34", "C35", "C37", "C40", "C43", "C45", "C50", "C51", "C53", "C59", "C61", "C63", "C64", "C65", "C67"], axis=1)
-
-# Transform all columns into float64
 for i in list(normal): 
     normal[i]=normal[i].apply(lambda x: str(x).replace("," , "."))
 normal = normal.astype(float)
 
-#from sklearn import preprocessing
-# https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=demian7607&logNo=222009975984
-#min_max_scaler = preprocessing.MinMaxScaler()
-min_max_scaler = preprocessing.StandardScaler()
+min_max_scaler = preprocessing.MinMaxScaler()
+
 x = normal.values
 x_scaled = min_max_scaler.fit_transform(x)
-#normal = pd.DataFrame(x_scaled)
-#normal = pd.DataFrame(x)
+normal = pd.DataFrame(x_scaled)
 
-# PCA
-pca = PCA(n_components=51)
-printcipalComponents = pca.fit_transform(x_scaled)
-normal= pd.DataFrame(data=printcipalComponents)
-
-
-
-#Read data
-#SWaT
 attack = pd.read_csv("input/SWaT_Dataset_Attack_v0.csv",sep=";")#, nrows=1000)
+labels = [ float(label!= 'Normal' ) for label  in attack["Normal/Attack"].values]
 attack = attack.drop(["Timestamp" , "Normal/Attack" ] , axis = 1)
-#attack.shape
 
-#HAI
-#attack_dataset = sorted([x for x in Path('input_HAI/').glob("test*.csv")])
-#attack = dataframe_from_csvs(attack_dataset)
-#attack = attack.drop(["time", "attack_P1", "attack_P2", "attack_P3"], axis=1)
-#attack = attack.drop(["time"], axis=1)
-#attack = attack.drop(["time", "C02", "C03", "C06", "C08", "C09", "C10", "C11", "C14", "C18", "C19", "C21", "C22", "C33", "C34", "C35", "C37", "C40", "C43", "C45", "C50", "C51", "C53", "C59", "C61", "C63", "C64", "C65", "C67"], axis=1)
-
-
-
-# Transform all columns into float64
 for i in list(attack):
     attack[i]=attack[i].apply(lambda x: str(x).replace("," , "."))
 attack = attack.astype(float)
 
-
 x = attack.values 
 x_scaled = min_max_scaler.transform(x)
 attack = pd.DataFrame(x_scaled)
-#attack = pd.DataFrame(x)
 
-# PCA
-pca = PCA(n_components=51)
-printcipalComponents = pca.fit_transform(x_scaled)
-attack = pd.DataFrame(data=printcipalComponents)
-
-
-window_size=8
-
+window_size=12
 
 windows_normal=normal.values[np.arange(window_size)[None, :] + np.arange(normal.shape[0]-window_size)[:, None]]
-#print("window normal shape", windows_normal.shape)
-windows_attack=attack.values[np.arange(window_size)[None, :] + np.arange(attack.shape[0]-window_size)[:, None]]
-#print("window attack shape", windows_attack.shape)
+windows_normal.shape
 
-import torch.utils.data as data_utils
+windows_attack=attack.values[np.arange(window_size)[None, :] + np.arange(attack.shape[0]-window_size)[:, None]]
+windows_attack.shape
 
 BATCH_SIZE =  7919
-N_EPOCHS = 300
-hidden_size = 18
+N_EPOCHS = 100
+hidden_size = 100
 
 w_size=windows_normal.shape[1]*windows_normal.shape[2]
 z_size=windows_normal.shape[1]*hidden_size
 
-windows_normal_train = windows_normal[:int(np.floor(.8 * .5 * windows_normal.shape[0]))]
-windows_normal_val = windows_normal[int(np.floor(.8 * .5 * windows_normal.shape[0])):int(np.floor(.5 * windows_normal.shape[0]))]
-windows_normal_test = windows_normal[int(np.floor(.5 * windows_normal.shape[0])):]
+windows_normal_train = windows_normal[:int(np.floor(.8 *  windows_normal.shape[0]))]
+windows_normal_val = windows_normal[int(np.floor(.8 *  windows_normal.shape[0])):int(np.floor(windows_normal.shape[0]))]
 
 train_loader = torch.utils.data.DataLoader(data_utils.TensorDataset(
     torch.from_numpy(windows_normal_train).float().view(([windows_normal_train.shape[0],w_size]))
@@ -120,29 +72,23 @@ val_loader = torch.utils.data.DataLoader(data_utils.TensorDataset(
 ) , batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
 
 test_loader = torch.utils.data.DataLoader(data_utils.TensorDataset(
-    torch.from_numpy(np.concatenate([windows_normal_test,windows_attack])).float().view(([windows_normal_test.shape[0]+windows_attack.shape[0],w_size]))
+    torch.from_numpy(windows_attack).float().view(([windows_attack.shape[0],w_size]))
 ) , batch_size=BATCH_SIZE, shuffle=False, num_workers=0)
 
-
 model = UsadModel(w_size, z_size)
-#print(model) see model info
 model = to_device(model,device)
 
 history = training(N_EPOCHS,model,train_loader,val_loader)
 
-plot_history(history)
-
+#plot_history(history)
 
 torch.save({
             'encoder': model.encoder.state_dict(),
             'decoder1': model.decoder1.state_dict(),
             'decoder2': model.decoder2.state_dict()
-            }, "model_swat.pth")
+            }, "model.pth")
 
-
-
-# after model load, re-train HAI and test
-checkpoint = torch.load("model_swat.pth")
+checkpoint = torch.load("model.pth")
 
 model.encoder.load_state_dict(checkpoint['encoder'])
 model.decoder1.load_state_dict(checkpoint['decoder1'])
@@ -150,53 +96,26 @@ model.decoder2.load_state_dict(checkpoint['decoder2'])
 
 results=testing(model,test_loader)
 
+windows_labels=[]
+for i in range(len(labels)-window_size):
+    windows_labels.append(list(np.int_(labels[i:i+window_size])))
+
+label = [1.0 if (np.sum(window) > 0) else 0 for window in windows_labels]
+y_test = label
 y_pred=np.concatenate([torch.stack(results[:-1]).flatten().detach().cpu().numpy(),
                               results[-1].flatten().detach().cpu().numpy()])
-threshold = 1
-# threshold = 0.9 this is best
-#y_pred_ = np.zeros(y_pred.shape[0])
-#y_pred_[y_pred >= threshold] = 1
-y_test=np.concatenate([np.zeros(windows_normal_test.shape[0]),
-                       np.ones(windows_attack.shape[0])])
-#print("normal_test\n", windows_normal_test.shape[0], "attack\n", windows_attack.shape[0])
 
-#histogram(y_test,y_pred)
+threshold=ROC(y_test,y_pred)
+print("len", len(y_pred), len(y_test))
 
-#threshold=ROC(y_test,y_pred)
+y_pred_ = adjust_predicts(y_pred, label, threshold)
+y_pred_ = put_labels(y_pred_, threshold)
 
-#confusion_matrix(y_test, np.where(y_pred > threshold, 1, 0))
-#confusion_matrix(y_test,np.where(y_pred > threshold, 1, 0),perc=True)
-#print("threshold : ", threshold)
-test_labels = put_labels(y_pred, threshold)
-print("recall", recall_score(y_test,test_labels))
-print("precision", precision_score(y_test, test_labels))
-print("f1", f1_score(y_test, test_labels))
-
-
-### HAI
-
-
-
-TaPR = etapr.evaluate_haicon(anomalies=y_test, predictions=test_labels)
-print(f"F1: {TaPR['f1']:.3f} (TaP: {TaPR['TaP']:.3f}, TaR: {TaPR['TaR']:.3f})")
-print(f"# of detected anomalies: {len(TaPR['Detected_Anomalies'])}")
-print(f"Detected anomalies: {TaPR['Detected_Anomalies']}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("recall", recall_score(y_test,y_pred_))
+print("precision", precision_score(y_test, y_pred_))
+print("f1", f1_score(y_test, y_pred_))
+tn, fp, fn, tp =  confusion_matrix(y_test, y_pred_).ravel()
+print("tn fp fn tp : ", tn, fp, fn, tp)
 
 
 
